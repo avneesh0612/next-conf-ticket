07025ccdbf649be6320e319683eb25ddb5198f83
Your Name <avneeshagarwal0612@gmail.com>
2021-09-22 19:11:44 +0530

    built the ticket

--- pages/index.js
+++ pages/index.js
@@ -1,82 +1,86 @@
-import Head from 'next/head'
+import Head from "next/head";
+import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+    <div className="flex flex-col items-center min-h-screen text-white bg-black">
       <Head>
         <title>Create Next App</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
-      <main className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center">
-        <h1 className="text-6xl font-bold">
-          Welcome to{' '}
-          <a className="text-blue-600" href="https://nextjs.org">
-            Next.js!
-          </a>
-        </h1>
-
-        <p className="mt-3 text-2xl">
-          Get started by editing{' '}
-          <code className="p-3 font-mono text-lg bg-gray-100 rounded-md">
-            pages/index.js
-          </code>
-        </p>
-
-        <div className="flex flex-wrap items-center justify-around max-w-4xl mt-6 sm:w-full">
-          <a
-            href="https://nextjs.org/docs"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Documentation &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Find in-depth information about Next.js features and API.
-            </p>
-          </a>
-
-          <a
-            href="https://nextjs.org/learn"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Learn &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Learn about Next.js in an interactive course with quizzes!
-            </p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Examples &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Discover and deploy boilerplate example Next.js projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Deploy &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+      <div className="flex flex-col items-center justify-center hover:animate-shake">
+        <div className="relative z-50 w-20 h-80 -mb-28">
+          <Image layout="fill" objectFit="contain" src="/tag.svg" />
         </div>
-      </main>
-
-      <footer className="flex items-center justify-center w-full h-24 border-t">
-        <a
-          className="flex items-center justify-center"
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
+        <div
+          style={{
+            background:
+              "url(https://res.cloudinary.com/dssvrf9oz/image/upload/v1632305278/bg_zhs6oa.png)",
+          }}
+          className="w-72  border-4 relative !bg-no-repeat !bg-cover !bg-center border-pink rounded-[40px] overflow-hidden"
         >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className="h-4 ml-2" />
-        </a>
-      </footer>
+          <div className="w-2/5 h-3 mx-auto mt-5 border-2 rounded-full border-pink"></div>
+          <div className="p-1">
+            <div className="flex items-center gap-5 mt-5 ml-5 ">
+              <Image
+                width={75}
+                height={75}
+                className="object-cover w-48 rounded-full"
+                src="https://github.com/avneesh0612.png"
+                alt=""
+              />
+              <div className="flex flex-col justify-around">
+                <h1 className="text-xl font-bold">Avneesh</h1>
+                <h1 className="-mt-2 text-xl font-bold">Agarwal</h1>
+                <p className="text-sm ">@avneesh0612</p>
+              </div>
+            </div>
+
+            <div className="relative h-24 mx-auto w-60">
+              <Image
+                layout="fill"
+                objectFit="contain"
+                src="https://raw.githubusercontent.com/gist/avneesh0612/c14a081aabda6b920df747da83919a16/raw/51f06024134c0e26ceb24e1214f511c263a81c82/next.svg"
+                alt="next"
+              />
+            </div>
+            <div className="-mt-2">
+              <div className="flex flex-col items-center justify-center">
+                <p className="text-base font-bold">October 26, 2021</p>
+                <p className="text-sm font-medium text-center">
+                  9a-1p PT Online
+                </p>
+              </div>
+
+              <div className="flex items-center px-20 mx-auto mt-10 mb-5">
+                <h1 className="text-base font-medium text-center">Hosted by</h1>
+                <svg
+                  width="18"
+                  height="16"
+                  viewBox="0 0 25 23"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="ml-2"
+                >
+                  <path
+                    d="M12.5 0L24.1913 22.5H0.808657L12.5 0Z"
+                    fill="white"
+                  />
+                </svg>
+              </div>
+            </div>
+          </div>
+          <div
+            style={{
+              background:
+                "url(https://res.cloudinary.com/dssvrf9oz/image/upload/v1632305277/id-bg_obziaz.png)",
+            }}
+            className="!bg-cover flex items-center justify-center py-8 -mb-1 text-center relative"
+          >
+            <span className="font-mono text-4xl font-medium">#001755</span>
+          </div>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,38 @@
 module.exports = {
-  mode: 'jit',
-  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  mode: "jit",
+  purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        pink: "#e02960",
+      },
+      animation: {
+        shake: "shake 2s 1",
+      },
+      keyframes: {
+        shake: {
+          "20%": {
+            transform: "rotate(2.333deg)",
+          },
+          "40%": {
+            transform: "rotate(-2deg)",
+          },
+          "60%": {
+            transform: "rotate(0.667deg)",
+          },
+          "80%": {
+            transform: "rotate(-0.333deg)",
+          },
+          "100%": {
+            transform: "rotate(0deg)",
+          },
+        },
+      },
+    },
   },
   variants: {
     extend: {},
   },
   plugins: [],
-}
+};
